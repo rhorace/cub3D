2e1ad2d899b3761300bc83a04e1484ca48a3f74f
Rodrigue HORACE <rhorace@student.42.fr>
2026-08-17 14:06:43 +0200

    a la norme

--- sources/parsing/get_the_color.c
+++ sources/parsing/get_the_color.c
@@ -6,7 +6,7 @@
 /*   By: rhorace <rhorace@student.42paris.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 14:02:01 by rhorace           #+#    #+#             */
-/*   Updated: 2026/08/13 16:26:06 by rhorace          ###   ########.fr       */
+/*   Updated: 2026/08/17 13:46:50 by rhorace          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,74 @@ int	between_0_255(t_color *couleur)
 		&& (couleur->b >= 0 && couleur->b <= 255));
 }
 
-int	get_color(t_game *cub3d, char *line)
+static int	which_part(t_game *cub3d, t_color color, char flag)
+{
+	if (!between_0_255(&color))
+		return (-1);
+	if (flag == 'F')
+	{
+		if (floor_ready(&cub3d->floor))
+			return (-1);
+		cub3d->floor = color;
+	}
+	else if (flag == 'C')
+	{
+		if (ceiling_ready(&cub3d->ceiling))
+			return (-1);
+		cub3d->ceiling = color;
+	}
+	return (1);
+}
+
+static int	get_number(char *line, int *i)
+{
+	int		start;
+	int		n;
+	char	*number;
+
+	start = *i;
+	if (!ft_isdigit(line[start]))
+		return (-1);
+	while (ft_isdigit(line[*i]))
+		(*i)++;
+	number = ft_substr2(line, start, *i);
+	if (!number)
+		return (-1);
+	n = ft_atoi(number);
+	free (number);
+	while (line[*i] == ' ' || line[*i] == '\t')
+		(*i)++;
+	return (n);
+}
+
+int	get_rgb(t_game *cub3d, char *line, int *i, char flag)
 {
 	t_color	color;
+
+	color.r = get_number(line, i);
+	if (line[*i] != ',')
+		return (-1);
+	(*i)++;
+	while (line[*i] == ' ' || line[*i] == '\t')
+		(*i)++;
+	color.g = get_number(line, i);
+	if (line[*i] != ',')
+		return (-1);
+	(*i)++;
+	while (line[*i] == ' ' || line[*i] == '\t')
+		(*i)++;
+	color.b = get_number(line, i);
+	if (line[*i] != '\n' && line[*i] != '\0')
+		return (-1);
+	if (which_part(cub3d, color, flag) == -1)
+		return (-1);
+	return (1);
+}
+
+int	get_color(t_game *cub3d, char *line)
+{
 	int		i;
 	char	flag;
-	char	*number;
-	int		start;
 
 	if (!cub3d || !line)
 		return (-1);
@@ -42,61 +103,7 @@ int	get_color(t_game *cub3d, char *line)
 		i++;
 	if (!line[i] || line[i] == '\n')
 		return (-1);
-	start = i;
-	if (!ft_isdigit(line[start]))
+	if (get_rgb(cub3d, line, &i, flag) == -1)
 		return (-1);
-	while (ft_isdigit(line[i]))
-		i++;
-	number = ft_substr2(line, start, i);
-	color.r = ft_atoi(number);
-	free(number);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	if (line[i] != ',')
-		return (-1);
-	i++;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	start = i;
-	if (!ft_isdigit(line[start]))
-		return (-1);
-	while (ft_isdigit(line[i]))
-		i++;
-	number = ft_substr2(line, start, i);
-	color.g = ft_atoi(number);
-	free(number);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	if (line[i] != ',')
-		return (-1);
-	i++;
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	start = i;
-	if (!ft_isdigit(line[start]))
-		return (-1);
-	while (ft_isdigit(line[i]))
-		i++;
-	number = ft_substr2(line, start, i);
-	color.b = ft_atoi(number);
-	free(number);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
-	if (line[i] != '\n' && line[i] != '\0')
-		return (-1);
-	if (!between_0_255(&color))
-		return (-1);
-	if (flag == 'F')
-	{
-		if (floor_ready(&cub3d->floor))
-			return (-1);
-		cub3d->floor = color;
-	}
-	else if (flag == 'C')
-	{
-		if (ceiling_ready(&cub3d->ceiling))
-			return (-1);
-		cub3d->ceiling = color;
-	}
 	return (1);
 }
